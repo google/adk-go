1b6326f4a4444cb17657f3fc1a2882ebb4dd6804
Anastasiia Dudarenko <adudarenko@google.com>
2025-09-12 13:38:36 +0000

    Add license header to restapi package

--- cmd/restapi/main.go
+++ cmd/restapi/main.go
@@ -1,22 +1,27 @@
-// Package main is the entry point for the REST API server.
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
 
-	"google.golang.org/adk/agent/llmagent"
 	"google.golang.org/adk/cmd/restapi/handlers"
 	"google.golang.org/adk/cmd/restapi/routers"
 	"google.golang.org/adk/cmd/restapi/utils"
-	"google.golang.org/adk/llm/gemini"
-	"google.golang.org/adk/tool"
-	"google.golang.org/adk/tool/geminitool"
-	"google.golang.org/genai"
 )
 
 func corsWithArgs(serverArgs utils.AdkAPIArgs) func(next http.Handler) http.Handler {
@@ -34,53 +39,8 @@ func corsWithArgs(serverArgs utils.AdkAPIArgs) func(next http.Handler) http.Hand
 	}
 }
 
-func initAdk() {
-	ctx := context.Background()
-	model, err := gemini.NewModel(ctx, "gemini-2.0-flash", &genai.ClientConfig{
-		APIKey: os.Getenv("GEMINI_API_KEY"),
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	agent, err := llmagent.New(llmagent.Config{
-		Name:        "weather_time_agent",
-		Model:       model,
-		Description: "Agent to answer questions about the time and weather in a city.",
-		Instruction: "I can answer your questions about the time and weather in a city.",
-		Tools: []tool.Tool{
-			geminitool.GoogleSearch{},
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	agent2, err := llmagent.New(llmagent.Config{
-		Name:        "foobar",
-		Model:       model,
-		Description: "Agent to answer questions about the time and weather in a city.",
-		Instruction: "I can answer your questions about the time and weather in a city.",
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Agents created: %v, %v", agent, agent2)
-
-	// adkApi := adk.NewAPIImpl()
-
-	// adkApi.SessionService = sessionservice.Mem()
-	// adkApi.AddModel(model)
-	// adkApi.AddAgent(agent)
-	// adkApi.AddAgent(agent2)
-	// return adkApi
-}
-
 func main() {
-
 	serverArgs := utils.ParseArgs()
-	initAdk()
 
 	log.Printf("Starting server on port %d with front address %s", serverArgs.Port, serverArgs.FrontAddress)
 
