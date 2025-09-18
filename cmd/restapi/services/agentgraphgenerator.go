54154b6d903166eb5589e76ee2669af8d2e3dc6c
Anastasiia Dudarenko <adudarenko@google.com>
2025-09-18 13:36:32 +0000

    go lint fixes

--- cmd/restapi/services/agentgraphgenerator.go
+++ cmd/restapi/services/agentgraphgenerator.go
@@ -1,3 +1,17 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package services
 
 import (
@@ -163,8 +177,12 @@ func buildGraph(graph *gographviz.Graph, instance any, highlightedPairs [][]stri
 
 func GetAgentGraph(ctx context.Context, agent agent.Agent, highlightedPairs [][]string) (string, error) {
 	graph := gographviz.NewGraph()
-	graph.SetName("AgentGraph")
-	graph.SetDir(true)
+	if err := graph.SetName("AgentGraph"); err != nil {
+		return "", err
+	}
+	if err := graph.SetDir(true); err != nil {
+		return "", err
+	}
 	if err := graph.AddAttr(graph.Name, "rankdir", "LR"); err != nil {
 		return "", err
 	}
