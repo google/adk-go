936bc121c40a8af7db979562c07d0017306afa0d
Karol Droste <kdroste@google.com>
2025-10-23 08:35:59 +0000

    Moved webui files

---
+++ cmd/launcher/webui/webui.go
@@ -0,0 +1,59 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package webui
+
+import (
+	"embed"
+	"io/fs"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"google.golang.org/adk/cmd/launcher/adk"
+	"google.golang.org/adk/cmd/restapi/handlers"
+)
+
+// embed web UI files into the executable
+
+//go:embed distr/*
+var content embed.FS
+
+func AddSubrouter(router *mux.Router, pathPrefix string, adkConfig *adk.Config, backendAddress string) *mux.Router {
+	// Setup serving of ADK Web UI
+	rUi := router.Methods("GET").PathPrefix("/ui/").Subrouter()
+
+	//   generate /assets/config/runtime-config.json in the runtime.
+	//   It removes the need to prepare this file during deployment and update the distribution files.
+	runtimeConfigResponse := struct {
+		BackendUrl string `json:"backendUrl"`
+	}{BackendUrl: backendAddress}
+	rUi.Methods("GET").Path("/assets/config/runtime-config.json").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		handlers.EncodeJSONResponse(runtimeConfigResponse, http.StatusOK, w)
+	})
+
+	//   redirect the user from / to /ui/
+	router.Methods("GET").Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/ui/", http.StatusFound)
+	})
+
+	// serve web ui from the embedded resources
+	ui, err := fs.Sub(content, "distr")
+	if err != nil {
+		log.Fatalf("cannot prepare ADK Web UI files as embedded content: %v", err)
+	}
+	rUi.Methods("GET").Handler(http.StripPrefix("/ui/", http.FileServer(http.FS(ui))))
+
+	return rUi
+}
