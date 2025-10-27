8e73840670d3b99adccec0d64264892c481e05d4
Karol Droste <kdroste@google.com>
2025-10-27 09:24:19 +0000

    Added WebUI launcher

--- cmd/launcher/webui/webui.go
+++ cmd/launcher/webui/webui.go
@@ -16,23 +16,78 @@ package webui
 
 import (
 	"embed"
+	"flag"
+	"fmt"
 	"io/fs"
 	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"google.golang.org/adk/cmd/launcher"
 	"google.golang.org/adk/cmd/launcher/adk"
 	"google.golang.org/adk/cmd/restapi/handlers"
 )
+
+// WebUIConfig contains parametres for lauching ADK Web UI
+type WebUIConfig struct {
+	backendAddress string
+	pathPrefix     string
+}
+
+// ApiLauncher can launch ADK Web UI
+type WebUILauncher struct {
+	flags  *flag.FlagSet
+	config *WebUIConfig
+}
+
+// FormatSyntax implements web.WebSublauncher.
+func (w *WebUILauncher) FormatSyntax() string {
+	return launcher.FormatFlagUsage(w.flags)
+}
+
+// Keyword implements web.WebSublauncher.
+func (w *WebUILauncher) Keyword() string {
+	return "webui"
+}
+
+// Parse implements web.WebSublauncher.
+func (w *WebUILauncher) Parse(args []string) ([]string, error) {
+	err := w.flags.Parse(args)
+	if err != nil || !w.flags.Parsed() {
+		return nil, fmt.Errorf("failed to parse webui flags: %v", err)
+	}
+	restArgs := w.flags.Args()
+	return restArgs, nil
+}
+
+// SetupRoutes implements web.WebSublauncher.
+func (w *WebUILauncher) SetupRoutes(router *mux.Router, adkConfig *adk.Config) {
+	// no need to modify top level routes
+}
+
+// SetupSubrouters implements web.WebSublauncher.
+func (w *WebUILauncher) SetupSubrouters(router *mux.Router, adkConfig *adk.Config) {
+	w.AddSubrouter(router, w.config.pathPrefix, adkConfig, w.config.backendAddress)
+}
+
+// SimpleDescription implements web.WebSublauncher.
+func (w *WebUILauncher) SimpleDescription() string {
+	return "starts ADK Web UI server which provides UI for interacting with ADK REST API"
+}
+
+// UserMessage implements web.WebSublauncher.
+func (w *WebUILauncher) UserMessage(webUrl string, printer func(v ...any)) {
+	printer(fmt.Sprintf("       webui:  you can access API using %s%s", webUrl, w.config.pathPrefix))
+}
 
 // embed web UI files into the executable
 
 //go:embed distr/*
 var content embed.FS
 
-func AddSubrouter(router *mux.Router, pathPrefix string, adkConfig *adk.Config, backendAddress string) {
+func (w *WebUILauncher) AddSubrouter(router *mux.Router, pathPrefix string, adkConfig *adk.Config, backendAddress string) {
 	// Setup serving of ADK Web UI
-	rUi := router.Methods("GET").PathPrefix("/ui/").Subrouter()
+	rUi := router.Methods("GET").PathPrefix(pathPrefix).Subrouter()
 
 	//   generate /assets/config/runtime-config.json in the runtime.
 	//   It removes the need to prepare this file during deployment and update the distribution files.
@@ -43,9 +98,9 @@ func AddSubrouter(router *mux.Router, pathPrefix string, adkConfig *adk.Config, 
 		handlers.EncodeJSONResponse(runtimeConfigResponse, http.StatusOK, w)
 	})
 
-	//   redirect the user from / to /ui/
+	//   redirect the user from / to pathPrefix (/ui/)
 	router.Methods("GET").Path("/").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		http.Redirect(w, r, "/ui/", http.StatusFound)
+		http.Redirect(w, r, pathPrefix, http.StatusFound)
 	})
 
 	// serve web ui from the embedded resources
@@ -53,6 +108,18 @@ func AddSubrouter(router *mux.Router, pathPrefix string, adkConfig *adk.Config, 
 	if err != nil {
 		log.Fatalf("cannot prepare ADK Web UI files as embedded content: %v", err)
 	}
-	rUi.Methods("GET").Handler(http.StripPrefix("/ui/", http.FileServer(http.FS(ui))))
+	rUi.Methods("GET").Handler(http.StripPrefix(pathPrefix, http.FileServer(http.FS(ui))))
+}
 
+func NewLauncher() *WebUILauncher {
+	config := &WebUIConfig{}
+
+	fs := flag.NewFlagSet("webui", flag.ContinueOnError)
+	fs.StringVar(&config.backendAddress, "api_server_address", "http://localhost:8080/api", "ADK REST API server address as seen from the user browser. Please specify the whole URL, i.e. 'http://localhost:8080/api'.")
+	config.pathPrefix = "/ui/"
+
+	return &WebUILauncher{
+		config: config,
+		flags:  fs,
+	}
 }
