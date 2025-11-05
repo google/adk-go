1815e9c8f78b133952c3c67542f1886feb738d53
westerberg <westerberg@google.com>
2025-11-05 13:56:11 +0000

    Change dbsession state to validate key and use session.ErrStateKeyNotExist; Add session package documentation.

--- session/session.go
+++ session/session.go
@@ -23,38 +23,69 @@ import (
 	"google.golang.org/adk/model"
 )
 
+// Session represents a series of interactions between a user and agents.
 type Session interface {
+	// ID returns the unique identifier of the session.
 	ID() string
+	// AppName returns name of the app.
 	AppName() string
+	// UserID returns the id of the user.
 	UserID() string
 
+	// State returns the state of the session.
 	State() State
+	// Events return the events of the session, e.g. user input, model response, function call/response, etc.
 	Events() Events
+	// LastUpdateTime returns the time of the last update.
 	LastUpdateTime() time.Time
 }
 
+// State defines a standard interface for a key-value store.
+// It provides basic methods for accessing, modifying, and iterating over
+// key-value pairs.
 type State interface {
+	// Get retrieves the value associated with a given key.
+	// It return a ErrStateKeyNotExist error if the key does not exist.
 	Get(string) (any, error)
+	// Set assigns the given value to the given key, overwriting any
+	// existing value. It returns an error if the underlying storage
+	// operation fails.
 	Set(string, any) error
+	// All returns an iterator (iter.Seq2) that yields all key-value pairs
+	// currently in the state. The order of iteration is not guaranteed.
 	All() iter.Seq2[string, any]
 }
 
+// ReadonlyState defines a standard interface for a key-value store.
+// It provides basic methods for accessing, and iterating over
+// key-value pairs.
 type ReadonlyState interface {
+	// Get retrieves the value associated with a given key.
+	// It return a ErrStateKeyNotExist error if the key does not exist.
 	Get(string) (any, error)
+	// All returns an iterator (iter.Seq2) that yields all key-value pairs
+	// currently in the state. The order of iteration is not guaranteed.
 	All() iter.Seq2[string, any]
 }
 
+// Events defines a standard interface for an Event list.
+// It provides methods for iterating over the sequence and accessing
+// individual events by their index.
 type Events interface {
+	// All returns an iterator (iter.Seq) that yields all events
+	// in the sequence, preserving their order.
 	All() iter.Seq[*Event]
+	// Len returns the total number of events in the sequence.
 	Len() int
+	// At returns the event at the specified index i.
 	At(i int) *Event
 }
 
 // TODO: Clarify what fields should be set when Event is created/processed.
 // TODO: Verify if we can hide Event completely; how Agents work with events.
 // TODO: Potentially expose as user-visible event or layer.
-//
-// Event represents an even in a conversation between agents and users.
+
+// Event represents an interaction in a conversation between agents and users.
 // It is used to store the content of the conversation, as well as
 // the actions taken by the agents like function calls, etc.
 type Event struct {
@@ -74,6 +105,7 @@ type Event struct {
 	// Branch is used when multiple sub-agent shouldn't see their peer agents'
 	// conversation history.
 	Branch string
+	// Author is the name of the event's author
 	Author string
 
 	// The actions taken by the agent.
@@ -84,7 +116,7 @@ type Event struct {
 	LongRunningToolIDs []string
 }
 
-// NewEvent creates a new event.
+// NewEvent creates a new event defining now as the timestamp.
 func NewEvent(invocationID string) *Event {
 	return &Event{
 		ID:           uuid.NewString(),
@@ -93,6 +125,8 @@ func NewEvent(invocationID string) *Event {
 		Actions:      EventActions{StateDelta: make(map[string]any)},
 	}
 }
+
+// TODO: Set by clients?
 
 // EventActions represents the actions attached to an event.
 type EventActions struct {
@@ -103,8 +137,6 @@ type EventActions struct {
 	// value is the version.
 	ArtifactDelta map[string]int64
 
-	// TODO: Set by clients?
-	//
 	// If true, it won't call model to summarize function response.
 	// Only valid for function response event.
 	SkipSummarization bool
@@ -114,10 +146,12 @@ type EventActions struct {
 	Escalate bool
 }
 
+// Prefixes for defining session's state scopes
 const (
 	KeyPrefixApp  string = "app:"
 	KeyPrefixTemp string = "temp:"
 	KeyPrefixUser string = "user:"
 )
 
+// ErrStateKeyNotExist defines the error thrown by State Get when key does not exist
 var ErrStateKeyNotExist = errors.New("state key does not exist")
