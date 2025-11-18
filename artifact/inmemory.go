e0fee389055519b6667748610a7806319a521d23
Menish Chandra <72362333+mekch01@users.noreply.github.com>
2025-11-18 23:38:38 +0530

    Remove copyright and license comments from inmemory.go
    Removed copyright notice and license information from inmemory.go.

--- artifact/inmemory.go
+++ artifact/inmemory.go
@@ -1,35 +1,4 @@
-// Copyright 2025 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
 
-package artifact
-
-import (
-	"context"
-	"fmt"
-	"io/fs"
-	"iter"
-	"maps"
-	"math"
-	"slices"
-	"sort"
-	"strings"
-	"sync"
-
-	"google.golang.org/genai"
-	"rsc.io/omap"
-	"rsc.io/ordered"
-)
 
 // inMemoryService is an in-memory implementation of the Service.
 // It is primarily for testing and demonstration purposes.
