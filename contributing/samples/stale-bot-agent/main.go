864ef5f7a2429f49a9173f2f1c7f5748e7286a93
Induri Reddy <indurireddy@google.com>
2026-02-11 10:04:00 +0000

    fix-lint-issues-1

--- contributing/samples/stale-bot-agent/main.go
+++ contributing/samples/stale-bot-agent/main.go
@@ -1,10 +1,10 @@
-//    Copyright 2026 Google LLC
+// Copyright 2025 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -25,6 +25,8 @@ import (
 	"sync"
 	"time"
 
+	"google.golang.org/genai"
+
 	"google.golang.org/adk/agent"
 	"google.golang.org/adk/agent/llmagent"
 	"google.golang.org/adk/artifact"
@@ -34,7 +36,6 @@ import (
 	"google.golang.org/adk/session"
 	"google.golang.org/adk/tool"
 	"google.golang.org/adk/tool/functiontool"
-	"google.golang.org/genai"
 )
 
 // --- Configuration Constants ---
