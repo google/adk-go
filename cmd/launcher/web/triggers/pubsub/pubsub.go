14c28a68ba890de6db02148e8357623583b12cbb
Anastasiia Dudarenko <adudarenko@google.com>
2026-04-08 08:42:33 +0000

    Adjust flag values

--- cmd/launcher/web/triggers/pubsub/pubsub.go
+++ cmd/launcher/web/triggers/pubsub/pubsub.go
@@ -71,17 +71,17 @@ func (p *pubsubLauncher) Parse(args []string) ([]string, error) {
 	if err != nil || !p.flags.Parsed() {
 		return nil, fmt.Errorf("failed to parse pubsub flags: %v", err)
 	}
-	if p.config.triggerMaxRetries < 0 {
-		return nil, fmt.Errorf("trigger_max_retries must be >= 0")
+	if p.config.triggerMaxRetries <= 0 {
+		return nil, fmt.Errorf("trigger_max_retries must be > 0")
 	}
 	if p.config.triggerBaseDelay < 0 {
 		return nil, fmt.Errorf("trigger_base_delay must be >= 0")
 	}
-	if p.config.triggerMaxDelay < 0 {
-		return nil, fmt.Errorf("trigger_max_delay must be >= 0")
+	if p.config.triggerMaxDelay <= 0 {
+		return nil, fmt.Errorf("trigger_max_delay must be > 0")
 	}
-	if p.config.triggerMaxRuns < 0 {
-		return nil, fmt.Errorf("trigger_max_concurrent_runs must be >= 0")
+	if p.config.triggerMaxRuns <= 0 {
+		return nil, fmt.Errorf("trigger_max_concurrent_runs must be > 0")
 	}
 
 	prefix := p.config.pathPrefix
