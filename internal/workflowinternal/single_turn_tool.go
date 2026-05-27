e1c00c122bee25b559c3fc4f2fbc0c3ca7339211
Dmitry Pasiukevich <20398573+dpasiukevich@users.noreply.github.com>
2026-05-27 10:57:23 +0200

    feat: add llmagent mode api (#900)
    * feat: add llmagent mode api

---
+++ internal/workflowinternal/single_turn_tool.go
@@ -0,0 +1,11 @@
+package workflowinternal
+
+import (
+	"google.golang.org/adk/agent"
+	"google.golang.org/adk/tool"
+)
+
+func NewSingleTurnTool(a agent.Agent) (tool.Tool, error) {
+	// TODO: implement
+	return nil, nil
+}
