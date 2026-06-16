62c0057ae5c322f5781b2e38df66849496aa542b
Karol Droste <kdroste@google.com>
2026-06-16 10:35:55 +0000

    Cleaning up internal

--- internal/workflowinternal/single_turn_tool_test.go
+++ internal/workflowinternal/single_turn_tool_test.go
@@ -226,7 +226,6 @@ func TestSingleTurnTool_Run_HappyPath(t *testing.T) {
 	)
 	orchestrator := workflow.NewDynamicNode("orchestrator",
 		func(ctx workflow.NodeContext, _ string, _ func(*session.Event) error) (any, error) {
-			// ic := ctx.WithContext(workflow.WithNodeContext(ctx, ctx))
 			toolCtx := agent.NewToolContext(ctx, "fc-id", &session.EventActions{}, nil)
 			gotResult, gotErr = st.Run(toolCtx, map[string]any{"request": "hello"})
 			return nil, gotErr
