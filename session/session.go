d4e1264f48e0037aa062996dcf81f721619a7017
wojtasjan <wojtasjan@google.com>
2026-08-10 14:51:31 +0000

    remove doc.go files where unnecessary and move package comments

--- session/session.go
+++ session/session.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package session provides types to manage user sessions and their states.
 package session
 
 import (
