53a6ac828fc7c97167bc20fc1e8c30b19ca063e9
wolo <wolo@google.com>
2026-09-12 10:43:55 +0000

    fix(adkrest): serve the endpoints the ADK web UI actually calls (#1459)
    (cherry picked from commit 40b4993b06b696837898401399985f5c27c5b3c8)

--- server/adkrest/handler.go
+++ server/adkrest/handler.go
@@ -44,7 +44,7 @@ func NewServer(cfg ServerConfig) (*Server, error) {
 	}
 
 	router := mux.NewRouter().StrictSlash(true)
-	router.HandleFunc("/health", healthHandler).Methods(http.MethodGet)
+	router.HandleFunc("/health", healthHandler).Methods(http.MethodGet, http.MethodHead)
 	// TODO: Allow taking a prefix to allow customizing the path
 	// where the ADK REST API will be served.
 	setupRouter(router,
@@ -53,6 +53,9 @@ func NewServer(cfg ServerConfig) (*Server, error) {
 		routers.NewAppsAPIRouter(controllers.NewAppsAPIController(cfg.AgentLoader)),
 		routers.NewDebugAPIRouter(controllers.NewDebugAPIController(cfg.SessionService, cfg.AgentLoader, debugTelemetry)),
 		routers.NewArtifactsAPIRouter(controllers.NewArtifactsAPIController(cfg.ArtifactService)),
+		routers.NewVersionAPIRouter(controllers.NewVersionAPIController()),
+		routers.NewAgentGraphAPIRouter(controllers.NewAgentGraphAPIController(cfg.AgentLoader)),
+		&routers.TestsAPIRouter{},
 		&routers.EvalAPIRouter{},
 	)
 	return &Server{
